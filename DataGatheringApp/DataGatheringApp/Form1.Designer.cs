3fb0bc6f5d3c9e7a76e88755a5d2212e0ca4b390
LegionV2 <rassko@live.com>
2016-04-04 20:58:38 +0200

    Basic Layout of page one. The very beginning.

---
+++ DataGatheringApp/DataGatheringApp/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace DataGatheringApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Q1_Label = new System.Windows.Forms.Label();
+            this.Q1Desc_Label = new System.Windows.Forms.Label();
+            this.Q1_Options = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 482);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Q1_Label
+            // 
+            this.Q1_Label.AutoSize = true;
+            this.Q1_Label.Location = new System.Drawing.Point(30, 28);
+            this.Q1_Label.Name = "Q1_Label";
+            this.Q1_Label.Size = new System.Drawing.Size(58, 13);
+            this.Q1_Label.TabIndex = 1;
+            this.Q1_Label.Text = "Question 1";
+            // 
+            // Q1Desc_Label
+            // 
+            this.Q1Desc_Label.AutoSize = true;
+            this.Q1Desc_Label.Location = new System.Drawing.Point(30, 41);
+            this.Q1Desc_Label.Name = "Q1Desc_Label";
+            this.Q1Desc_Label.Size = new System.Drawing.Size(114, 13);
+            this.Q1Desc_Label.TabIndex = 2;
+            this.Q1Desc_Label.Text = "Question 1 Description";
+            // 
+            // Q1_Options
+            // 
+            this.Q1_Options.FormattingEnabled = true;
+            this.Q1_Options.Items.AddRange(new object[] {
+            "Option 1",
+            "Option 2",
+            "Option 3"});
+            this.Q1_Options.Location = new System.Drawing.Point(33, 58);
+            this.Q1_Options.Name = "Q1_Options";
+            this.Q1_Options.Size = new System.Drawing.Size(111, 49);
+            this.Q1_Options.TabIndex = 3;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Option 1",
+            "Option 2",
+            "Option 3"});
+            this.checkedListBox1.Location = new System.Drawing.Point(33, 158);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(111, 49);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Question 1 Description";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Question 1";
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Option 1",
+            "Option 2",
+            "Option 3"});
+            this.checkedListBox2.Location = new System.Drawing.Point(33, 250);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(111, 49);
+            this.checkedListBox2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Question 1 Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Question 1";
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "Option 1",
+            "Option 2",
+            "Option 3"});
+            this.checkedListBox3.Location = new System.Drawing.Point(33, 351);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(111, 49);
+            this.checkedListBox3.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 334);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Question 1 Description";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Question 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 517);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Q1_Options);
+            this.Controls.Add(this.Q1Desc_Label);
+            this.Controls.Add(this.Q1_Label);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Q1_Label;
+        private System.Windows.Forms.Label Q1Desc_Label;
+        private System.Windows.Forms.CheckedListBox Q1_Options;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
